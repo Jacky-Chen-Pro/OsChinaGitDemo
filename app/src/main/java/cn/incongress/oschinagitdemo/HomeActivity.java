5db476052cca59785077f839890158a9225157b6
Jacky-Chen-Pro <qi12371@gmail.com>
2016-03-23 16:39:51 +0800

    添加启动页面

---
+++ app/src/main/java/cn/incongress/oschinagitdemo/HomeActivity.java
@@ -0,0 +1,15 @@
+package cn.incongress.oschinagitdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Jacky on 2016/3/23.
+ */
+public class HomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
